aa7aa553635de770e5fde44f47ec51bdd12c4da1
Muskaan Jain <jmuskaan1999@gmail.com>
2019-06-23 16:12:40 +0530

    using port from env if available

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express =require("express")
 const path =require ("path")
 
+const server_port=process.env.PORT || 3333
+
 const app=express()
 app.use('/public',express.static(path.join(__dirname,'public')))
 app.get('/', (req , res) => res.send("hello from backend"))
 
-app.listen(3333,()=> console.log("server started"))
+app.listen(server_port,()=> console.log("server started"))
